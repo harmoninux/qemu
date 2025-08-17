958780aa34480b0fe38e01857e57f010c1c3206d
hackeris <hackeris@qq.com>
2025-08-18 01:52:26 +0800

    support mount root of host to guest

--- include/qemu/path.h
+++ include/qemu/path.h
@@ -2,8 +2,8 @@
 #define QEMU_PATH_H
 
 void init_paths(const char *prefix);
-char *relocate_path_at(int dirfd, const char *name, char *out, bool follow_symlink);
-char *restore_path(const char* name, char* out);
+char *relocate_path_at(int host_dirfd, const char *guest_path, char *out, bool follow_symlink);
+char *restore_path(const char* host_path, char* out);
 char* resolve_with_path_env(const char* path_env, int dirfd, const char* name, char* out);
 char* resolve_abs_with_cwd(const char* path, char* out);
 
